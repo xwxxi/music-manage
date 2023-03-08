8e2870797769df5329fbdb58645d81bcd82c497e
xwxxi <1923656649@qq.com>
2023-03-09 00:23:40 +0800

    1

--- src/router/index.js
+++ src/router/index.js
@@ -3,7 +3,7 @@ import Router from 'vue-router'
 
 Vue.use(Router)
 
-const router =new Router({
+const router = new Router({
   routes: [
     {
       path: '/',
@@ -17,44 +17,50 @@ const router =new Router({
           // 如果子路由的path的最前边有“/”，则不需要加父路由的路径即可访问此子组件。
           path: '/Info',
           component: resolve => require(['../pages/InfoPage.vue'], resolve),
-          meta:{isAuth:true}
+          meta: { isAuth: true }
         },
         {
           // 如果子路由的path的最前边有“/”，则不需要加父路由的路径即可访问此子组件。
           path: '/Consumer',
           component: resolve => require(['../pages/ConsumerPage.vue'], resolve),
-          meta:{isAuth:true}
+          meta: { isAuth: true }
         },
         {
           // 如果子路由的path的最前边有“/”，则不需要加父路由的路径即可访问此子组件。
           path: '/Singer',
           component: resolve => require(['../pages/SingerPage.vue'], resolve),
-          meta:{isAuth:true}
+          meta: { isAuth: true }
         },
         {
           // 如果子路由的path的最前边有“/”，则不需要加父路由的路径即可访问此子组件。
           path: '/SongList',
           component: resolve => require(['../pages/SongListPage.vue'], resolve),
-          meta:{isAuth:true}
+          meta: { isAuth: true }
         },
       ]
     }
   ]
 })
-// 路由守卫
+//全局前置路由，配合浏览器localStorage进行鉴权操作
 router.beforeEach((to, from, next) => {
-  console.log(to.meta.isAuth);
-  if(to.meta.isAuth){
+  //首先，我们先看一看to和from参数，next就是执行的意思，不写页面是不会跳转的
+  // console.log(to, from);
+  if (to.meta.isAuth) { //判断是否需要鉴权
     const tokenStr = localStorage.getItem('userName')
     if (tokenStr) {
       next()
     } else {
       // 没有token
-      next({path: '/'})
+      next({ path: '/' })
     }
-  }else{
+  } else {
     next()
   }
 })
+
+// 全局后置路由，简单写一个弹窗，在进入每一个路由组件后弹出一句话
+// router.afterEach(() =>{
+//   alert('欢迎你！！');
+// })
 
 export default router;
